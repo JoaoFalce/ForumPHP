cbe3e041de1c61f1413c1ffbb342c1e7b6772cfb
João Falce <101810976+JoaoFalce@users.noreply.github.com>
2025-06-16 12:36:15 -0300

    atualização do recover

--- app/Views/auth/recover.php
+++ app/Views/auth/recover.php
@@ -47,20 +47,5 @@
         </div>
     <?php endif; ?>
 </div>
-
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-    const cpfInput = document.querySelector('input[name="cpf"]');
-    if (cpfInput) {
-        cpfInput.addEventListener('input', function(e) {
-            let value = e.target.value.replace(/\D/g, '');
-            if (value.length > 11) {
-                value = value.substring(0, 11);
-            }
-            e.target.value = value;
-        });
-    }
-});
-</script>
 </body>
 </html>
