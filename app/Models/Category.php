4efbf4f8e826f591dd18e8fd48cf460c07337476
João Falce <101810976+JoaoFalce@users.noreply.github.com>
2025-06-16 16:16:18 -0300

    bug fix

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,20 +2,7 @@
 require_once __DIR__ . '/../../config/database.php';
 
 class Category {
-    
-    /*
-    public static function create($name) {
-        try {
-            $db = Database::connect();
-            $stmt = $db->prepare("INSERT INTO categories (name) VALUES (?)");
-            return $stmt->execute([$name]);
-        } catch (PDOException $e) {
-            error_log("Erro ao criar categoria: " . $e->getMessage());
-            return false;
-        }
-    }
 
-    */
     public static function getAll() {
         try {
             $db = Database::connect();
@@ -38,30 +25,5 @@ class Category {
             return false;
         }
     }
-      /*
-    public static function update($id, $name) {
-        try {
-            $db = Database::connect();
-            $stmt = $db->prepare("UPDATE categories SET name = ? WHERE id = ?");
-            return $stmt->execute([$name, $id]);
-        } catch (PDOException $e) {
-            error_log("Erro ao atualizar categoria: " . $e->getMessage());
-            return false;
-        }
-    }
-    */
-
-         /*
-    public static function delete($id) {
-        try {
-            $db = Database::connect();
-            $stmt = $db->prepare("DELETE FROM categories WHERE id = ?");
-            return $stmt->execute([$id]);
-        } catch (PDOException $e) {
-            error_log("Erro ao deletar categoria: " . $e->getMessage());
-            return false;
-        }
-    }
-        */
 }
 ?>
