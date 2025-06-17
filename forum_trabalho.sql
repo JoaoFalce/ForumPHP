909f9a084d6ae53e849f57f9e6b56bb2a3420a21
eloicesar <162243229+eloicesar@users.noreply.github.com>
2025-06-16 22:49:40 -0300

    Update forum_trabalho.sql

--- forum_trabalho.sql
+++ forum_trabalho.sql
@@ -12,20 +12,7 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Banco de dados: `forum_trabalho`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `categories`
---
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
@@ -33,20 +20,14 @@ CREATE TABLE `categories` (
   `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Extraindo dados da tabela `categories`
---
+
 
 INSERT INTO `categories` (`id`, `name`) VALUES
 (9, 'Anúncios'),
 (8, 'Dúvidas'),
 (7, 'Geral');
 
--- --------------------------------------------------------
 
---
--- Estrutura da tabela `comments`
---
 
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE `comments` (
@@ -57,11 +38,6 @@ CREATE TABLE `comments` (
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `posts`
---
 
 DROP TABLE IF EXISTS `posts`;
 CREATE TABLE `posts` (
@@ -72,11 +48,6 @@ CREATE TABLE `posts` (
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `post_categories`
---
 
 DROP TABLE IF EXISTS `post_categories`;
 CREATE TABLE `post_categories` (
@@ -84,11 +55,6 @@ CREATE TABLE `post_categories` (
   `category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `users`
---
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
@@ -101,107 +67,70 @@ CREATE TABLE `users` (
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Extraindo dados da tabela `users`
---
+
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `cpf`, `birth_date`, `created_at`) VALUES
 (1, 'luana', 'luana@example.com', '$2y$10$e0NRNxCt2Evz5TpPRW2b0u28nBcy0xtSzFKFzDQkxO0FUwM5h53pC', '12345678901', '1990-01-01', '2025-06-07 23:52:56');
 
---
--- Índices para tabelas despejadas
---
 
---
--- Índices para tabela `categories`
---
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `name` (`name`);
 
---
--- Índices para tabela `comments`
---
+
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`),
   ADD KEY `post_id` (`post_id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Índices para tabela `posts`
---
+
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Índices para tabela `post_categories`
---
+
 ALTER TABLE `post_categories`
   ADD PRIMARY KEY (`post_id`,`category_id`),
   ADD KEY `category_id` (`category_id`);
 
---
--- Índices para tabela `users`
---
+
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`),
   ADD UNIQUE KEY `cpf` (`cpf`);
 
---
--- AUTO_INCREMENT de tabelas despejadas
---
 
---
--- AUTO_INCREMENT de tabela `categories`
---
+
 ALTER TABLE `categories`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
---
--- AUTO_INCREMENT de tabela `comments`
---
+
+
 ALTER TABLE `comments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `posts`
---
+
 ALTER TABLE `posts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `users`
---
+
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- Restrições para despejos de tabelas
---
 
---
--- Limitadores para a tabela `comments`
---
 ALTER TABLE `comments`
   ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
---
--- Limitadores para a tabela `posts`
---
+
+
 ALTER TABLE `posts`
   ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 
--- Limitadores para a tabela `post_categories`
---
 ALTER TABLE `post_categories`
   ADD CONSTRAINT `post_categories_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `post_categories_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
